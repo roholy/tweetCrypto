b083e53e9bb9e2e5d21056bf54a390918057b602
roholy <mattwilps@gmail.com>
2021-03-18 21:50:16 -0400

    update analysis

--- analysis.py
+++ analysis.py
@@ -1,3 +1,29 @@
 import talib
+import requests
+import pandas as pd
 
-candle_names = talib.get_function_groups()['Pattern Recognition']
+# link for Bitcoin Data
+link = "https://dev-api.shrimpy.io/v1/exchanges/coinbasepro/candles?quoteTradingSymbol=BTC&baseTradingSymbol=XLM&interval=1H"
+
+# API request historical
+historical_get = requests.get(link)
+
+# access the content of historical api request
+historical_json = historical_get.json()
+
+# extract json data as dictionary
+historical_dict = historical_json['Data']
+
+# extract Final historical df
+df = pd.DataFrame(historical_dict,
+                             columns=['close', 'high', 'low', 'open', 'time', 'volumefrom', 'volumeto'],
+                             dtype='float64')
+
+# time column is converted to "YYYY-mm-dd hh:mm:ss" ("%Y-%m-%d %H:%M:%S")
+posix_time = pd.to_datetime(df['time'], unit='s')
+
+# append posix_time
+df.insert(0, "Date", posix_time)
+
+# drop unix time stamp
+df.drop("time", axis = 1, inplace = True)
--- date.py
+++
@@ -1,3 +0,0 @@
-from datetime import datetime
-my_date = datetime.now()
-print(my_date.isoformat())
